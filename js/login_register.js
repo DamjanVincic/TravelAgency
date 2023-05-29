4a583a52dd1491d9fe100deef22af17df09cacdf
DamjanVincic <damjanvincic2003@gmail.com>
2023-05-29 11:39:31 +0200

    Added register functionality and form validation.

---
+++ js/login_register.js
@@ -0,0 +1,83 @@
+var registerForm = document.getElementById("registerForm");
+var usernameRegister = document.getElementById("usernameRegister");
+var passwordRegister = document.getElementById("passwordRegister");
+var emailRegister = document.getElementById("email");
+var phoneRegister = document.getElementById("phone");
+
+registerForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (registerForm.checkValidity()) {
+        var data = new FormData(event.target);
+
+        user = {}
+        for (obj of data) {
+            let [key, value] = obj
+            user[key] = value
+        }
+
+        fetch(`${databaseURL}korisnici.json`, {
+            method: 'POST',
+            body: JSON.stringify(user)
+        })
+        .then(resp => {
+            if (resp.ok) {
+                let registerModal = document.getElementById("registerModal");
+                // registerModal.classList.remove("show");
+                // registerModal.classList.add("hide");
+                registerModal.getElementsByClassName("btn-close")[0].click();
+
+                var alertHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    Uspesna registracija.
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>`;
+                document.getElementById("registerAlertPlaceholder").innerHTML = alertHTML;
+            } else {
+                window.location.replace("error.html");
+            }
+        })
+    } else {
+        registerForm.classList.add("was-validated")
+    }
+});
+
+usernameRegister.addEventListener("input", () => {
+    if (usernameRegister.value.indexOf(' ') >= 0) {
+        usernameRegister.setCustomValidity("error");
+    }
+    else {
+        usernameRegister.setCustomValidity("");
+    }
+})
+
+passwordRegister.addEventListener("input", () => {
+    if (passwordRegister.value.length < 8) {
+        passwordRegister.setCustomValidity("error");
+    }
+    else {
+        passwordRegister.setCustomValidity("");
+    }
+})
+
+emailRegister.addEventListener("input", () => {
+    if (!validateEmail(emailRegister.value))
+        emailRegister.setCustomValidity("error");
+    else
+        emailRegister.setCustomValidity("");
+})
+
+phoneRegister.addEventListener("input", () => {
+    if (isNaN(phoneRegister.value)) {
+        phoneRegister.setCustomValidity("error");
+    }
+    else {
+        phoneRegister.setCustomValidity("");
+    }
+})
+
+const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
